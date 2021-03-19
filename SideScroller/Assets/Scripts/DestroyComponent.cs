d29794c4467ebb2064536d35e97435b2b36f831a
Gavin Pack <gavinpack@gmail.com>
2021-03-18 23:48:42 -0600

    Added DestroyComponent Unity Tutorial Script

---
+++ SideScroller/Assets/Scripts/DestroyComponent.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DestroyComponent : MonoBehaviour
+{
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(GetComponent<MeshRenderer>());
+        }
+    }
+}
