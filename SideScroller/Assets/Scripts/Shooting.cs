e23cad6b072acaa294549d8b47c3692004a4cce7
Gavin Pack <gavinpack@gmail.com>
2021-03-30 23:53:56 -0600

    Added Shooting Unity Tutorial Script

---
+++ SideScroller/Assets/Scripts/Shooting.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    public Transform firePosition;
+    public float bulletSpeed;
+    
+    
+    private Inventory inventory;
+    
+    
+    void Awake ()
+    {
+        inventory = GetComponent<Inventory>();
+    }
+    
+    
+    void Update ()
+    {
+        Shoot();
+    }
+    
+    
+    void Shoot ()
+    {
+        if(Input.GetButtonDown("Fire1") && inventory.myStuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            inventory.myStuff.bullets--;
+        }
+    }
+}
