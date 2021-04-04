f6fb294d2c6db84213c7cbbc6fbbad37b8ee6e18
Gavin Pack <gavinpack@gmail.com>
2021-04-03 23:21:04 -0600

    Added InvokeScript Unity Tutorial Script

---
+++ SideScroller/Assets/Scripts/InvokeScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class InvokeScript : MonoBehaviour
+{
+    public GameObject target;
+    
+    
+    void Start()
+    {
+        Invoke ("SpawnObject", 2);
+    }
+    
+    void SpawnObject()
+    {
+        Instantiate(target, new Vector3(0, 2, 0), Quaternion.identity);
+    }
+}
