7ff948929f884abd7db6d848bd22ab50f5d040bd
Gavin Pack <gavinpack@gmail.com>
2021-04-14 23:44:45 -0600

    Added ButtonInput Unity Tutorial Script with Commented Errors

---
+++ SideScroller/Assets/Scripts/ButtonInput.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ButtonInput : MonoBehaviour
+{
+    //GUITexture is obsolete and breaks Unity
+    
+    //public GUITexture graphic;
+    public Texture2D standard;
+    public Texture2D downgfx;
+    public Texture2D upgfx;
+    public Texture2D heldgfx;
+    
+    void Start()
+    {
+        //graphic.texture = standard;
+    }
+    
+    void Update ()
+    {
+        bool down = Input.GetButtonDown("Jump");
+        bool held = Input.GetButton("Jump");
+        bool up = Input.GetButtonUp("Jump");
+        
+        if(down)
+        {
+            //graphic.texture = downgfx;
+        }
+        else if(held)
+        {
+            //graphic.texture = heldgfx;
+        }
+        else if(up)
+        {
+            //graphic.texture = upgfx;
+        }
+        else
+        {
+            //graphic.texture = standard;
+        }
+    
+        //guiText.text = " " + down + "\n " + held + "\n " + up;
+    }
+}
