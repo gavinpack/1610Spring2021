7060223523ad7b97103a7b24072b9b03b53a9479
Gavin Pack <gavinpack@gmail.com>
2021-04-17 23:56:04 -0600

    Added DualAxisExample Unity Tutorial Script with Commented Errors

---
+++ SideScroller/Assets/Scripts/DualAxisExample.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DualAxisExample : MonoBehaviour
+{
+    //GUIText is obsolete and breaks Unity.
+    
+    public float range;
+    //public GUIText textOutput;
+    
+    
+    void Update () 
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float xPos = h * range;
+        float yPos = v * range;
+        
+        transform.position = new Vector3(xPos, yPos, 0);
+        //textOutput.text = "Horizontal Value Returned: "+h.ToString("F2")+"\nVertical Value Returned: "+v.ToString("F2");    
+    }
+}
